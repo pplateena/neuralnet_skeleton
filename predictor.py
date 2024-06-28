a6c3c77eaf6b23c86dee7e67074313b08d7d25eb
ADHDPSYCH <h417rtc@gmail.com>
2024-06-28 17:00:56 +0300

    xvfd

--- predictor.py
+++ predictor.py
@@ -4,15 +4,12 @@ import tensorflow as tf
 
 from tensorflow.keras.optimizers import Adam
 from utility_modules.capture import capture_mode
-
+import utility_modules.move_ctype  as cici
 import cv2
 import numpy as np
-import pandas as pd
+
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Input, Lambda
-from sklearn.model_selection import train_test_split
+
 from tensorflow.keras.optimizers import Adam
 
 model_rap = tf.keras.models.load_model('C:\\Users\\platy_move\\Documents\\GitHub\\neuralnet_skeleton\\cemetery.h5')
@@ -21,7 +18,7 @@ model_rap.summary()
 model_rap.compile(loss='mean_squared_error', optimizer=Adam())
 
 
-for n in range(10):
+for n in range(40):
     img = capture_mode('desired', (0,0,1920,1080))
     resized = cv2.resize(img,(640,360))
     tbp = np.asarray(resized)
@@ -29,3 +26,16 @@ for n in range(10):
 
     prediction = model_rap.predict(tbp)
     print(prediction[0])
+    x,y,button = round(prediction[0]*3), round(prediction[1]*3), prediction[0]
+
+    cici.move_cursor_steps(x,y)
+
+    if button >= 50:
+        cici.press_right_button()
+        sleep(0.1)
+        cici.release_right_button()
+    else:
+        cici.press_left_button()
+        sleep(0.1)
+        cici.release_left_button()
+
