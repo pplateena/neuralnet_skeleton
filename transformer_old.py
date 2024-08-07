d6f29166f2c774d72b7469958d23935dccd1adb2
pplateena <ukrstar38@gmail.com>
2024-08-07 15:18:16 +0300

    documentation of transformer
    first gen of manual dataset classifier

--- transformer_old.py
+++ transformer_old.py
@@ -1,13 +1,26 @@
 import cv2
 import os
 import pandas as pd
-def scalar_mousepos(mx, my):
-    # mx, my = position()
-    scalar_mx, scalar_my = round(mx/1920, 4) ,round(my/1080, 4)
-    return scalar_mx, scalar_my
+"""
+THIS IS FIRST GENERATION OF CLASSIFIER, USED ONLY AS REFERENCE FOR FUTURE SCRIPTS.
 
-def parse_mfile(filename):
+"""
+def create_df(saved_folder = 'prep_data'):
+    """
 
+    :param saved_folder: any specified folder, prep_data by default
+    :return: creates a .csv dataframe with required format stored in specified location
+    """
+    df = pd.DataFrame(columns=["filename", "scalar_x", "scalar_y", "s_button"])
+    df.to_csv(f"{saved_folder}/image_data.csv", index=False)
+def parse_mfile(filename:str):
+    """
+    Function to return data captured from filename
+    :param filename: string of filename which is worked on
+    :return: mouse button that were pressed on given screenshot, x and y coordinates of mouse during screenshot
+
+    Example: filename = 'mouse_button.RMB_960_540_1241247.png
+    """
 
     parts = filename.split("_")
     if len(parts) != 5:
@@ -17,15 +30,38 @@ def parse_mfile(filename):
     y = int(parts[3])
 
     return side, x, y
-
+def scalar_mousepos(mx, my):
+    """
+    Function to convert mouse coordinates from monitor pixel range into scalar values
+    :param mx:
+    :param my:
+    :return: scalar_mx, scalar_my: range(float(0,1))
+    """
+    scalar_mx, scalar_my = round(mx/1920, 4) ,round(my/1080, 4)
+    return scalar_mx, scalar_my
 def draw_dot(image,button, x, y, radius=20, thickness=-1):
+    """
+    Function to draw dot on running cv2 image besed on button pressed
+    :param image: np.array() of image
+    :param button: 'left' or 'right' str()
+    :param x: int
+    :param y: int
+    :param radius: int
+    :param thickness: int
+    :return: None
+    """
     color = (255, 0, 0) if button == "left" else (0, 0, 255)
     cv2.circle(image, (x, y), radius, color, thickness)
+def sort_data(folder_path = "dataset/"):
+    """
+    Function to sort data by pressing buttons in cv2 window.
+    Moves images to different folders based on key pressed.
 
-def sort_data():
-    folder_path = "dataset/"
+    :param folder_path: path(str)
+    :return: None
+    """
+
     killer = 0
-
     delete_list = []
     approve_list = []
 
@@ -75,22 +111,20 @@ def sort_data():
     for filename in delete_list:
         os.remove(f'dataset/{filename}')
         print(f"Deleted {filename}")
-
     for filename in approve_list:
-
         os.replace(f'dataset/{filename}', f'prep_data/{filename}')  # Use replace for move functionality
         print(f"Moved {filename} to prep_data")
-sort_data()
-def create_df():
-    df = pd.DataFrame(columns=["filename", "scalar_x", "scalar_y", "s_button"])
-    df.to_csv("prep_data/image_data.csv", index=False)
 
-# create_df()
-def prepare_data():
-    folder_path = "prep_data/"
-    df = pd.read_csv("prep_data/image_data.csv")
+def prepare_data(folder_path = "prep_data/", df = pd.read_csv("prep_data/image_data.csv")):
+    """
+    Function to prepare data. Resizes fhd images to 640x480 and saves them, appending new x, y values and
+    pressed button information to dataframe
+    :param folder_path: path(str)
+    :param df: pandas dataframe
+    :return:
+    """
+    counter = 2300 # used to skip certain amount of already saved photos to exclude conflicts
 
-    counter = 2300
     for filename in os.listdir(folder_path):
         if filename.endswith(".jpg") and filename.startswith("M_"):
             button, x, y = parse_mfile(filename)
@@ -100,17 +134,20 @@ def prepare_data():
             resized_image = cv2.resize(image, (640, 360))
             cv2.imwrite(folder_path + filename, resized_image)
             scalar_x, scalar_y = round(x/3), round(y/3),
+
             s_button = 0 if button == "left" else 100
             print(scalar_x, scalar_y,s_button, filename)
 
-            # df.loc[counter] = [f'{counter}.jpg', scalar_x, scalar_y, s_button]
+            df.loc[counter] = [f'{counter}.jpg', scalar_x, scalar_y, s_button]
             os.rename(f'prep_data/{filename}', f'prep_data/{counter}.jpg')
             counter += 1
 
-    df.to_csv("prep_data/image_data.csv", index=False)
-prepare_data()
-
+    df.to_csv(f"{folder_path}/image_data.csv", index=False)
 def check_prep():
+    """
+    function to manually check whether stored images are relevant
+    :return: None
+    """
     folder_path = "prep_data/"
     df = pd.read_csv("prep_data/image_data.csv")
     print(df)
@@ -128,5 +165,10 @@ def check_prep():
 
                 cv2.imshow('rap', display)
                 key = cv2.waitKey(0) & 0xFF
+
+# create_df()
+
+prepare_data()
+sort_data()
 
 # check_prep()
